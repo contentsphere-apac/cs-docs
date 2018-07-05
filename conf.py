ad2598b9462afd0abdd05b79842deb2378203036
Ramon John Angostora <34431277+ramonangostora@users.noreply.github.com>
2018-07-05 19:18:00 +0800

    .
    .

--- conf.py
+++ conf.py
@@ -140,7 +140,7 @@ suppress_warnings = ['app.add_directive']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["sphinx_rtd_theme/static"]
+html_static_path = ["_theme/contentspheredocs/static"]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -164,7 +164,7 @@ html_static_path = ["sphinx_rtd_theme/static"]
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ["sphinx_rtd_theme"]
+html_theme_path = ["_theme"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
