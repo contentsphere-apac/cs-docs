dcf99069641562a1af93ee90d7b23797dda05226
BrianOrlanes <34431260+BrianOrlanes@users.noreply.github.com>
2018-07-05 15:50:00 +0800

    Revert "added new structure"
    This reverts commit b546d760019f62246bed017a6ba9a885747fc35c.

--- conf.py
+++ conf.py
@@ -72,7 +72,7 @@ source_suffix = '.txt'
 master_doc = 'contents'
 
 # General substitutions.
-project = 'Contentserv'
+project = 'Contentsphere'
 copyright = 'CS Group'
 
 
@@ -139,29 +139,13 @@ suppress_warnings = ['app.add_directive']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "contentspheredocs"
 html_static_path = ["_theme/contentspheredocs/_static"]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options =
-
-{
-    'canonical_url': 'http://help.contentserv.com//en/latest/',
-    'analytics_id': '',
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
-}
+# html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ["_theme"]
